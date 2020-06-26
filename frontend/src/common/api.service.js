943fa1dc58e646bff51c3a40f53bab1a26c787bd
Anas Ahmed <anasahmed700@gmail.com>
2020-06-26 21:50:44 +0500

    added API Service with fetch & CSRF Token

---
+++ frontend/src/common/api.service.js
@@ -0,0 +1,22 @@
+import {CSRFTOKEN} from './csrf_token'
+
+async function getJSON(response){
+    if (response.status === 204) return '';
+    return response.json();
+}
+
+function apiService(endpoint, method, data) {
+    const config = {
+        method: method || GET,
+        body: data !== undefined ? JSON.stringify(data) : null,
+        headers: {
+            'content-type': 'application/json',
+            'X-CSRFTOKEN': CSRFTOKEN
+        }
+    }
+    return fetch(endpoint, config)
+    .then(getJSON)
+    .catch(error => console.log(error))
+}
+
+export default APIService
